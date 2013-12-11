1ece48b5d4f9618fd986261fd8f11eb4a4c2a670
AmeliaMN <amelia.a.mcnamara@gmail.com>
2013-12-11 12:07:40 -0800

    violin plots

--- code/tweetplot.R
+++ code/tweetplot.R
@@ -20,14 +20,14 @@ p <- ggplot() + geom_histogram(aes(x=dates), binwidth=60*60*24*7)+ labs(title="A
 p <- p + ylab("Tweets") + ggtitle("One thousand tweets by @AmeliaMN") + xlab("")
 p <- p + scale_x_datetime(breaks="3 month", minor_breaks="1 month", labels=date_format("%B %Y"))
 p <- p +  annotate("text", x = ymd("2012 Dec 15"), y = 205, label = "#compj") +annotate("text", x = ymd("2013 Apr 01"), y = 310, label = "#eyeo2013")+annotate("text", x = ymd("2013 Sep 01"), y = 50, label = "#futureofstats")
-ggsave("1000tweets.pdf", p, width=20, height=6)
+ggsave("plots/1000tweets.pdf", p, width=20, height=6)
 
 
 r <- ggplot() + geom_line(aes(x=dates, y=1000:1))
 r <- r + ylab("Total number of tweets") + xlab("") + scale_x_datetime(breaks="1 year", minor_breaks="3 month", labels=date_format("%Y"))
 r <- r + ggtitle("Total number of tweets by @AmeliaMN over time")
 r
-ggsave("cumulativetweets.pdf", r)
+ggsave("plots/cumulativetweets.pdf", r)
 
 
 
@@ -39,7 +39,7 @@ s + annotate("text", x = 40000, y = 10, label = "@VirginAmerica") + annotate("te
 s + annotate("segment", x = 12000, xend = 12550, y = 15, yend = 10)
 s <- s + ggtitle("Number of tweets per user followed by @AmeliaMN")
 s <- s + scale_x_continuous(breaks=seq(from=0, to=150000, by=25000), minor_breaks=seq(from=-10000, to=150000, by=5000))
-ggsave("following.pdf", s, width=12, height=8)
+ggsave("plots/following.pdf", s, width=12, height=8)
 
 s
 
@@ -50,10 +50,22 @@ t <- t + annotate("text", x = 112500, y = 4, label = "@znmeb") + annotate("text"
 t <- t + ggtitle("Number of tweets per user following @AmeliaMN")
 t <- t + scale_x_continuous(breaks=seq(from=0, to=150000, by=25000), minor_breaks=seq(from=-10000, to=150000, by=5000))
 
-ggsave("followers.pdf", t, width=12, height=8)
+ggsave("plots/followers.pdf", t, width=12, height=8)
 
 ggplot(followertweets) + geom_histogram(aes(x=tweets)) + xlab("total tweets") + ylab("users") + xlim(0, 5000)
 t
+
+# Boxplot
+followertweets$type <- "Follower"
+followtweets$type <- "Following"
+allusers <- rbind(followertweets, followtweets)
+
+u <- ggplot(allusers) + geom_boxplot(aes(type, tweets)) + xlab("") + ylab("Total tweets") 
+u <- u + ggtitle("Number of tweets per user followed by or following @AmeliaMN")
+u
+
+ggsave("plots/boxplots.pdf", u, width=12, height=8)
+
 
 
 # Grab Ramnath Vaidya's gist from github: https://gist.github.com/ramnathv/7793167
